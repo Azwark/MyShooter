407f945d62fcbaf14554cba208fe6d6c4cba6520
Azwark <30425457+Azwark@users.noreply.github.com>
2021-07-28 21:07:38 -0400

    Covid and enemies move faster

--- app/src/main/java/com/example/gauntlet/AlienChaseSpec.java
+++ app/src/main/java/com/example/gauntlet/AlienChaseSpec.java
@@ -6,7 +6,7 @@ class AlienChaseSpec extends ObjectSpec {
     // This is all the unique specifications
     // for an alien that chases the player
     private static final String tag = "Alien";
-    private static final String bitmapName = "alien_ship1";
+    private static final String bitmapName = "covid";
     private static final float speed = 4f;
     private static final PointF relativeScale =
             new PointF(15f, 15f);
